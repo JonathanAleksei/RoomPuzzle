585820983a9c62ea98c1da3bf015807a942c0cd8
jonathanaleksei <j.suarez.a.garcia@gmail.com>
2023-09-22 23:55:53 -0500

    *** Project created Unreal

---
+++ Source/RoomPuzzle/RoomPuzzleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoomPuzzleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMPUZZLE_API ARoomPuzzleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
